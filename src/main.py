a069b151b30ffe8fd8c7553b1d8ced61fff22242
Victor Klimov <4klimov@gmail.com>
2022-01-02 18:10:03 +0300

    refactor: add structure to the project

--- src/main.py
+++ src/main.py
@@ -1,19 +1,17 @@
 import fastapi
-import uvicorn
 import fastapi_chameleon
-from fastapi_chameleon import template
+import uvicorn
+
+from views import account, home, packages
+
 
 app = fastapi.FastAPI()
 
 fastapi_chameleon.global_init('templates')
 
-
-@app.get('/')
-@template(template_file='index.html')
-def index(user: str = 'anon'):
-    return {
-        'user_name': user
-    }
+app.include_router(home.router)
+app.include_router(account.router)
+app.include_router(packages.router)
 
 
 if __name__ == "__main__":
---
+++ src/views/home.py
@@ -0,0 +1,18 @@
+import fastapi
+from fastapi_chameleon import template
+
+
+router = fastapi.APIRouter()
+
+
+@router.get('/')
+@template(template_file='home/index.html')
+def index(user: str = 'anon'):
+    return {
+        'user_name': user
+    }
+
+
+@router.get('/about')
+def about():
+    return {}
